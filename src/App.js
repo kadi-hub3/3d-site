d71374a982b3de1df2d4d902380cf7c4ab2734cd
Kadi M <kicktheball1@gmail.com>
2021-06-01 17:22:07 +0200

    3d scrolling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import space from "./space.jpg";
 
 function App() {
   const scene = new THREE.Scene();
@@ -20,7 +22,7 @@ function App() {
 
   const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
   const material = new THREE.MeshStandardMaterial({
-    color: 0xff6347,
+    color: 0x102030,
   });
   const torus = new THREE.Mesh(geometry, material);
   scene.add(torus);
@@ -28,18 +30,146 @@ function App() {
   const pointLight = new THREE.PointLight(0xffffff);
   pointLight.position.set(5, 5, 5);
 
-  scene.add(pointLight);
+  const ambientLight = new THREE.AmbientLight(0xffffff);
+
+  scene.add(pointLight, ambientLight);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  const addStars = () => {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3)
+      .fill()
+      .map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    scene.add(star);
+  };
+
+  Array(400).fill().forEach(addStars);
+
+  const spaceTexture = new THREE.TextureLoader().load(space);
+  scene.background = spaceTexture;
+
+  //Move On Scrolling
+  const moveCamera = () => {
+    const t = document.body.getBoundingClientRect().top;
+
+    torus.rotation.x += 0.05;
+    torus.rotation.y += 0.075;
+    torus.rotation.z += 0.05;
+
+    camera.position.x = t * -0.01;
+    camera.position.y = t * -0.0002;
+    camera.position.z = t * -0.0002;
+  };
+  document.body.onscroll = moveCamera;
 
   const animate = () => {
     requestAnimationFrame(animate);
+
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.01;
+
+    controls.update();
+
     renderer.render(scene, camera);
   };
 
   animate();
-  return <div />;
+  return (
+    <div>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+      <h1>3D PORTFOLIO</h1>
+      <p>DESCRIPTON</p>
+    </div>
+  );
 }
 
 export default App;
